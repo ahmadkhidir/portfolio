655ae843dd57cfafd1ad14f524ccf51f356469c3
Ahmad Khidir <khidirahmad05@gmail.com>
2025-12-12 22:45:43 +0100

    fix: Update test to use correct PortfolioStack for SQS queue validation

--- tests/unit/test_uinlp_website_stack.py
+++ tests/unit/test_uinlp_website_stack.py
@@ -1,12 +1,12 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from uinlp_website.uinlp_website_stack import OysirsStack
+from portfolio.portfolio_stack import PortfolioStack
 
 
 def test_sqs_queue_created():
     app = core.App()
-    stack = OysirsStack(app, "oysirs")
+    stack = PortfolioStack(app, "portfolio")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
